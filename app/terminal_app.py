60e809956de810998166fc9b6ecabfb1352a2293
Amal Abeygunawardana <amalabey@gmail.com>
2023-06-13 23:59:28 +1000

    Add cmd input handler

--- app/terminal_app.py
+++ app/terminal_app.py
@@ -3,6 +3,9 @@ from textual.containers import Container, VerticalScroll
 from textual.widgets import Header, Static
 from textual.widgets import Input
 
+CMD_INPUT_ID = "cmd-input-text"
+CMD_OUTPUT_ID = "cmd-output-text"
+
 
 class TerminalApp(App):
     CSS_PATH = "app.css"
@@ -12,13 +15,19 @@ class TerminalApp(App):
         with Container(id="app-grid"):
             with Container(id="left-pane"):
                 with VerticalScroll(id="cmd-output-container"):
-                    for num in range(100):
-                        yield Static(".......cmd output.....")
+                    yield Static(".......cmd output.....", id=CMD_OUTPUT_ID)
                 with Container(id="cmd-input-container"):
-                    yield Input(placeholder=">", id="cmd-input-text")
+                    cmd_input = Input(placeholder=">", id=CMD_INPUT_ID)
+                    cmd_input.action_submit = self.action_submit
+                    yield cmd_input
             with VerticalScroll(id="right-pane"):
                 for number in range(100):
                     yield Static(f"Vertical layout, child {number}")
+
+    def action_submit(self):
+        cmd_input = self.query_one(f"#{CMD_INPUT_ID}")
+        cmd_output = self.query_one(f"#{CMD_OUTPUT_ID}")
+        cmd_output.update(f"cmd .... {cmd_input.value}")
 
 
 if __name__ == "__main__":
