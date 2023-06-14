448f9e6c6c544138e337478d83064e9cf76ab578
Amal Abeygunawardana <amalabey@gmail.com>
2023-06-14 23:12:37 +1000

    Basic run command works

--- app/debug_console.py
+++ app/debug_console.py
@@ -7,37 +7,56 @@ DEFAULT_IP = "0.0.0.0"
 
 
 class DebugConsole():
-    def __init__(self, ip=DEFAULT_IP, port=DEFAULT_LISTENER_PORT):
+    def __init__(self, ip=DEFAULT_IP, port=DEFAULT_LISTENER_PORT, repel=True):
         self.ip = ip
         self.port = port
+        self.repel = repel
 
     def listen(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((self.ip, self.port))
-        sock.listen(1)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.ip, self.port))
+        self.sock.listen(1)
         print("Waiting for break point hit... ")
-        conn, addr = sock.accept()
+        self.conn, addr = self.sock.accept()
         print('Connected to agent: ', addr)
         # Upgrade connection using pty
         # upgrade_cmd = "python3 -c \'import pty; pty.spawn(\"/bin/sh\")\'\n\n"
         # conn.send(upgrade_cmd.encode('utf-8'))
-        while True:
-            # Receive data from the target and get user input
-            data = conn.recv(1024)
-            if not data:
-                print("Connection closed")
-                sock.close()
-                break
+        if self.repel:
+            while True:
+                # Receive data from the target and get user input
+                data = self.conn.recv(1024)
+                if not data:
+                    print("Connection closed")
+                    self.sock.close()
+                    break
 
-            ans = data.decode()
-            sys.stdout.write(ans)
-            command = input()
+                ans = data.decode()
+                self.on_response(ans)
+                command = input()
 
-            # Send command
+                # Send command
+                command += "\n"
+                self.conn.send(command.encode())
+                time.sleep(0.2)
+
+                # Remove the output of the "input()" function
+                sys.stdout.write("\033[A" + ans.split("\n")[-1])
+
+    def on_response(self, response: str):
+        sys.stdout.write(response)
+
+    def send_command(self, command: str):
+        if self.conn:
             command += "\n"
-            conn.send(command.encode())
+            self.conn.send(command.encode())
             time.sleep(0.2)
 
-            # Remove the output of the "input()" function
-            sys.stdout.write("\033[A" + ans.split("\n")[-1])
+            data = self.conn.recv(1024)
+            if not data:
+                self.sock.close()
+                raise Exception("Connection Closed")
+
+            ans = data.decode()
+            self.on_response(ans)
---
+++ app/run_command.py
@@ -0,0 +1,114 @@
+import os
+import socket
+from app.azure_pipelines_client import AzurePipelinesClient
+from app.azure_repos_client import AzureReposClient
+from app.base_command import Command
+from app.debug_console import DebugConsole
+from app.local_agent import LocalAgent
+from app.local_git_repo import LocalGitRepository
+from app.pipeline_definition import PipelineDefinition
+from app.terminal_ui import TerminalUi
+
+RUN_CMD_TEXT = "#run"
+
+
+class RunCommand(Command):
+    def __init__(self,
+                 app: TerminalUi,
+                 org_url: str,
+                 project_name: str,
+                 pipeline_id: str,
+                 personal_access_token: str,
+                 repo_path: str,
+                 file_path: str,
+                 debug: bool) -> None:
+        super().__init__(app)
+        self.org_url = org_url
+        self.project_name = project_name
+        self.pipeline_id = pipeline_id
+        self.personal_access_token = personal_access_token
+        self.repo_path = repo_path
+        self.file_path = file_path
+        self.debug = debug
+
+    def start(self) -> None:
+        self.app.on_load = self.execute
+        self.app.on_cmd = self.handle_command
+        self.app.run()
+
+    def execute(self) -> None:
+        hostname = socket.gethostname()
+        identifier = hostname.replace(" ", "").lower()
+
+        # Start local agent container
+        local_agent = LocalAgent(self.org_url, self.personal_access_token,
+                                 identifier)
+        local_agent.start()
+
+        # Recreate the temp branch
+        ref_name, object_id = self._recreate_temp_branch()
+
+        # Manipulate pipeline yaml to point to local agent and add breakpoints
+        file_abs_path = os.path.join(self.repo_path, self.file_path)
+        pipeline_defition = PipelineDefinition(file_abs_path)
+        yaml_content = pipeline_defition.annotate_yaml(self.debug,
+                                                       local_agent.get_agent_name())
+
+        # Update remote file in the temp branch
+        azure_repos_client = AzureReposClient(self.org_url, self.project_name,
+                                              self.personal_access_token)
+        azure_repos_client.update_remote_file(ref_name, object_id, self.file_path,
+                                              yaml_content)
+
+        # Run the pipeline
+        azure_pipelines_client = AzurePipelinesClient(self.org_url, self.project_name,
+                                                      self.personal_access_token)
+        azure_pipelines_client.run_pipeline(self.pipeline_id, ref_name)
+
+        # Listen for a reverse shell as the debug console
+        if self.debug:
+            self.debug_console = DebugConsole(repel=False)
+            self.debug_console.on_response = self.handle_response
+            self.debug_console.listen()
+
+    def handle_response(self, response):
+        self.app.append_cmd_output("\n")
+        self.app.append_cmd_output(response)
+
+    def handle_command(self):
+        cmd_text = self.app.get_cmd_text()
+        if cmd_text == RUN_CMD_TEXT:
+            self.execute()
+        else:
+            self.debug_console.send_command(cmd_text)
+
+    def _recreate_temp_branch(self):
+        local_git_repo = LocalGitRepository(self.repo_path)
+        git_username = local_git_repo.get_git_username()
+        git_username_hash = self._generate_unique_int(git_username)
+        temp_branch_name = f"tmp-{self.pipeline_id}-{git_username_hash}"
+
+        # Delete the temp branch if it already exists
+        azure_repos_client = AzureReposClient(self.org_url, self.project_name,
+                                              self.personal_access_token)
+        existing_remote_ref = azure_repos_client.get_remote_branch(temp_branch_name)
+        if len(existing_remote_ref["value"]) > 0:
+            azure_repos_client.delete_branch(temp_branch_name,
+                                             existing_remote_ref["value"][0]["objectId"])
+
+        # Get the remote tracking branch for the currently active branch
+        active_branch_name = local_git_repo.get_active_branch_name()
+        remote_tracking_ref = azure_repos_client.get_remote_branch(active_branch_name)
+        if len(remote_tracking_ref["value"]) == 0:
+            raise Exception("Unable to find the remote tracking branch for the active branch. Please publish the branch")
+        object_id = remote_tracking_ref["value"][0]["objectId"]
+
+        # Create the new temp branch
+        temp_branch_ref = azure_repos_client.create_branch(temp_branch_name,
+                                                           object_id)
+        return (temp_branch_ref["value"][0]["name"],
+                temp_branch_ref["value"][0]["newObjectId"])
+
+    def _generate_unique_int(self, text):
+        number = sum(ord(c) for c in text)
+        return number
